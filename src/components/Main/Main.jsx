02037a0ae7a8897ebf089788f53e35553b5914ee
farid95g <f95guluzade@gmail.com>
2021-05-18 22:36:57 +0400

    main project structure and github pages package

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from "./Main.module.css";
+
+const Main = () => {
+  return (
+    <main className={classes.main}>
+      <h1>Main</h1>
+    </main>
+  );
+}
+
+export default Main;
