aadb65ab473f7c2f57c9ce936be5046b721983e6
farid95g <f95guluzade@gmail.com>
2021-05-19 16:56:54 +0400

    social network links extracted into separate component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import classes from "./Footer.module.css";
+import SocialLinks from "./../SocialLinks/SocialLinks";
 
 const Footer = () => {
   return (
     <footer className={classes.footer}>
-      <h1>Footer</h1>
+      <SocialLinks />
     </footer>
   );
 }
--- src/components/Header/MainNavbar/MainNavbar.jsx
+++ src/components/Header/MainNavbar/MainNavbar.jsx
@@ -1,13 +1,6 @@
 import React from "react";
+import SocialLinks from "./../../SocialLinks/SocialLinks";
 import classes from "./MainNavbar.module.css";
-import {
-  FaTwitter,
-  FaInstagram,
-  FaFacebookF,
-  FaPinterestP,
-  FaRss,
-  FaYoutube,
-} from "react-icons/fa";
 
 const MainNavbar = () => {
   return (
@@ -35,40 +28,7 @@ const MainNavbar = () => {
             </li>
           </ul>
         </div>
-        <div className={classes.socialLinks}>
-          <ul>
-            <li>
-              <a href="https://www.instagram.com/" target="blank">
-                <FaInstagram />
-              </a>
-            </li>
-            <li>
-              <a href="https://twitter.com/" target="blank">
-                <FaTwitter />
-              </a>
-            </li>
-            <li>
-              <a href="https://www.facebook.com/" target="blank">
-                <FaFacebookF />
-              </a>
-            </li>
-            <li>
-              <a href="https://www.pinterest.com/" target="blank">
-                <FaPinterestP />
-              </a>
-            </li>
-            <li>
-              <a href="https://rss.com/" target="blank">
-                <FaRss />
-              </a>
-            </li>
-            <li>
-              <a href="https://www.youtube.com/" target="blank">
-                <FaYoutube />
-              </a>
-            </li>
-          </ul>
-        </div>
+        <SocialLinks />
       </div>
     </nav>
   );
