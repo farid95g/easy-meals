8d9eabc3229598472f124d17e4d841eb43f5dce6
farid95g <f95guluzade@gmail.com>
2021-09-14 14:46:32 +0400

    Meal component hover action

--- src/components/Meal/Meal.jsx
+++ src/components/Meal/Meal.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import styles from "./Meal.module.css";
 import { BsClock } from "react-icons/bs";
-import { BiLike } from "react-icons/bi";
+import { BiLike, BiBookmark } from "react-icons/bi";
+import profilePicture from "./../../assets/images/profile.png";
 
 const Meal = (props) => {
   return (
@@ -10,6 +11,7 @@ const Meal = (props) => {
         <img src={props.background} />
       </div>
       <div className={styles.description}>
+        <div>
         <div className={styles.info}>
           <span className={styles.time}>
             <BsClock />
@@ -20,9 +22,27 @@ const Meal = (props) => {
             <span>SUPER EASY</span>
           </span>
         </div>
-        <p>
+        <p className={styles.title}>
           7 easy bruschetta recipes that look fancy
         </p>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet, consec tetu ipisicing elit, sed do eiusmod tempo inci didunt ut labore et dolore magna aliqut en.
+        </p>
+        <div className={styles.user}>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <div className={styles.profilePicture}>
+              <img src={profilePicture} alt="user profile picture" />
+            </div>
+            <div style={{ display: "flex", flexDirection: "column", marginLeft: "15px", marginTop: "-5px" }}>
+              <span className={styles.username}>MAGI DAWSON</span>
+              <span className={styles.entryDate}>May 6, 2020</span>
+            </div>
+          </div>
+          <div className={styles.bookmark}>
+            <BiBookmark />
+          </div>
+        </div>
+        </div>
       </div>
     </div>
   )
